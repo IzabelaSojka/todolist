da2ffae7bbfb8297d9f1e0d7de8cef8d3d9ab150
IzabelaSojka <sojkaizabela1@gmail.com>
2023-03-16 22:14:56 +0100

    Added check, edit, delete

--- todo.js
+++ todo.js
@@ -1,13 +1,18 @@
 const form = document.getElementById("todoform");
 const toDoInput = document.getElementById("new");
 const toDoList = document.getElementById("todos-list");
+const notificationElement = document.querySelector(".notification");
 
-let toDos = [];
+let toDos = JSON.parse(localStorage.getItem("toDos")) || [];
+let editToDoId = -1;
+
+renderToDo();
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
     saveToDo();
     renderToDo();
+    localStorage.setItem("toDos", JSON.stringify(toDos));
 });
 
 function saveToDo(){
@@ -16,33 +21,45 @@ function saveToDo(){
     const isDuplicate = toDos.some((toDo) => toDo.value.toUpperCase() === toDoValue.toUpperCase());
 
     if(isEmpty){
-        alert("Your list is empty!");
+        showNotification("Your list is empty!");
     }
     else if(isDuplicate){
-        alert("Already exist!");
+        showNotification("Already exist!");
     }
     else{
-        toDos.push({
+        if(editToDoId >= 0){
+            toDos = toDos.map((toDo, index) =>({
+                ...toDo,
+                value: index === editToDoId ? toDoValue : toDo.value,
+            }));
+            editToDoId = -1;
+        }
+        else{
+            toDos.push({
             value : toDoValue,
-            checked : false,
-            color : '#' + Math.floor(Math.random()*16777215).toString(16)
-        });
-
-        toDos.value = '';
+            checked : false
+            });
+        }
+         toDos.value = '';
     }
-
-    
 }
 
 function renderToDo(){
+    if(toDos.length === 0){
+        toDoList.innerHTML = `<center> Nothing to do!!!</center>`;
+        return;
+    }
     toDoList.innerHTML = '';
     toDos.forEach((toDo, index) => {
         toDoList.innerHTML += `
             <div class="todo" id=${index}>
-                <i class="bi ${toDo.checked ? 'bi-check-square' : 'bi-square'}"></i>
+                <i 
+                    class="bi ${toDo.checked ? 'bi-check-square' : 'bi-square'}"
+                    data-action="check"
+                ></i>
                 <p class="">${toDo.value}</p>
-                <i class="bi bi-pen-fill"></i>
-                <i class="bi bi-trash3-fill"></i>
+                <i class="bi bi-pen-fill" data-action="edit"></i>
+                <i class="bi bi-trash3-fill" data-action="delete"></i>
             </div>
         `;  
     });
@@ -52,10 +69,47 @@ toDoList.addEventListener('click', (event) => {
     const target = event.target;
     const parentEl = target.parentNode;
     
-    if (parentEl.className !== 'todo') return;g
+    if (parentEl.className !== 'todo') return;
 
     const toDo = parentEl;
     const toDoId = Number(toDo.id);
 
-    console.log(toDoId);
+    const action = target.dataset.action;
+
+    action === "check" && checkToDo(toDoId);
+    action === "edit" && editToDo(toDoId);
+    action === "delete" && deleteToDo(toDoId);
 });
+
+function checkToDo(toDoId){
+    toDos = toDos.map((toDo, index) => ({
+        ...toDo,
+        checked: index === toDoId ? !toDo.checked : toDo.checked,
+    }));
+
+    renderToDo();
+    localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
+function editToDo(toDoId){
+    toDoInput.value = toDos[toDoId].value;
+    editToDoId = toDoId;
+}
+
+function deleteToDo(toDoId){
+    toDos = toDos.filter((todo, index) => index !== toDoId);
+    editToDoId = -1;
+
+    renderToDo();
+    localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
+function showNotification(msg){
+    notificationElement.innerHTML = msg;
+
+    notificationElement.classList.add("notif-enter");
+
+    setTimeout(() =>{
+        notificationElement.classList.remove("notif-enter")
+    }, 4500)
+}
